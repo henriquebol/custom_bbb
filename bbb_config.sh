20f68bbd9e98ea9898df7bcb80dff2934e919ec9
henriquebol <henbolima@gmail.com>
2020-03-20 10:10:10 -0300

    Update bbb_config.sh

--- bbb_config.sh
+++ bbb_config.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# wget -O - https://raw.githubusercontent.com/henriquebol/custom_bbb/master/bbb_config.sh | sudo bash
+# sudo wget -O - https://raw.githubusercontent.com/henriquebol/custom_bbb/master/bbb_config.sh | sudo bash
 
+mkdir -p ~/logs
 readonly LOG=~/logs/config.log
 
 echo '$(date +%F\ %T) - Iniciando configuração...' >> $LOG
 cd ~/
-mkdir -p ~/logs
 
 # Fazendo backups
+echo '$(date +%F\ %T) - Iniciando Backups......' >> $LOG
 sudo cp  /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties.bck
 sudo cp /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml.bck
+echo '$(date +%F\ %T) - Backups OK' >> $LOG
 
 # Slide Default
 echo '$(date +%F\ %T) - Baixando Slide default...' >> $LOG
@@ -30,8 +32,9 @@ echo '$(date +%F\ %T) - Favicon - OK' >> $LOG
 
 # /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
 echo '$(date +%F\ %T) - Alterando bigbluebutton.properties...' >> $LOG
-sed -i -e 's/\(attendeesJoinViaHTML5Client=\).*/\1true/' \
+sudo sed -i -e 's/\(attendeesJoinViaHTML5Client=\).*/\1true/' \
 -e 's/\(moderatorsJoinViaHTML5Client=\).*/\1true/' \
+-e 's/\(bigbluebutton.web.logoutURL=\).*/\1 https://goo.gl/forms/PEB4WFixcWwuGyOt1/' \
 -e 's/\(defaultWelcomeMessage=\).*/\1<br><br>Bem-vindo \u00e0 <b>%%CONFNAME%%</b>!<br><br>Para participar da webconfer\u00eancia, seu computador deve ter, no m\u00ednimo, um <u>processador dual-core com pelo menos 2G de mem\u00f3ria, uma conex\u00e3o de 1Mbits de download e 0.5 Mbits de upload</u>. Voc\u00ea pode testar sua conex\u00e3o usando o <a href="http://www.speedtest.net" target="_blank"><u><b>speedtest.net</b></u></a>.<br><br>Se sua conex\u00e3o estiver boa, o \u00e1udio tiver sido configurado corretamente e, mesmo assim, o seu \u00e1udio apresentar problemas, tente alternar de navegador. S\u00e3o recomendadas as \u00faltimas vers\u00f5es dos navegadores <a href="https://www.mozilla.org/pt-BR/firefox/new/" target="_blank"><u>Firefox</u></a> ou <a href="https://www.google.com.br/chrome/browser/desktop/" target="_blank"><u>Chrome</u></a>.<br><br>Ao final da webconfer\u00eancia, clique em sair, no canto superior direito, e responda ao nosso question\u00e1rio de avalia\u00e7\u00e3o da ferramenta.<br><br>Qualquer d\u00favida na utiliza\u00e7\u00e3o da ferramenta, entre em contato atrav\u00e9s do email: webconferencia@virtual.ufc.br ou chame o <a href="http://solar.virtual.ufc.br/webconferences?request_support=true" target="_blank"><b>suporte em tempo real (clicando aqui)</b></a> ou no menu Ajuda./' \
 -e 's/\(defaultWelcomeMessageFooter=\).*/\1<br>/' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
 echo '$(date +%F\ %T) - Html5 default - OK' >> $LOG
@@ -39,7 +42,7 @@ echo '$(date +%F\ %T) - defaultWelcomeMessageFooter - OK' >> $LOG
 
 # /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
 echo '$(date +%F\ %T) - Alterando settings.yml...' >> $LOG
-sed -i -e 's/\(clientTitle:\).*/\1 Solar webconferência/' \
+sudo sed -i -e 's/\(clientTitle:\).*/\1 Solar webconferência/' \
 -e 's/\(copyright:\).*/\1 'Solar webconferência - ©2019 BigBlueButton Inc.'/' \
 -e 's/\(helpLink:\).*/\1 http:\/\/solar.virtual.ufc.br\/webconferences?request_support=true/' /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
 echo '$(date +%F\ %T) - clientTitle - OK' >> $LOG
